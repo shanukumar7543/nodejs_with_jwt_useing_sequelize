15c3668eb4849b00b60f7e952f2ac58a3b448dc9
shanu kumar <shanukumar7543@gmail.com>
2024-02-08 13:50:09 +0530

    timestamp added

--- winston/index.js
+++ winston/index.js
@@ -1,12 +1,18 @@
 const winston = require('winston')
+const { format, transports } = require('winston')
+const { combine, timestamp, label, printf } = format
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`
+})
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-
+  //   level: 'info',
+  //   format: winston.format.json(),
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
   transports: [
-    // new winston.transports.Console(),
-    // new winston.transports.File({ filename: 'fulllogs.log' }),
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'fulllogs.log' }),
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
   ],
